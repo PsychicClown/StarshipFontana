d605d40d406265a9e969f206b127b3e3e9f5dd63
Kyle Best <medievilwarrior@hotmail.com>
2016-04-21 15:45:38 +0100

    Added up and down movement

--- src/SFEvent.cpp
+++ src/SFEvent.cpp
@@ -19,6 +19,13 @@ SFEvent::SFEvent(const SDL_Event & event) {
     case SDLK_RIGHT:
       code = SFEVENT_PLAYER_RIGHT;
       break;
+// kyle's up code
+    case SDLK_UP:
+      code = SFEVENT_PLAYER_UP;
+      break;
+    case SDLK_DOWN:
+      code = SFEVENT_PLAYER_DOWN;
+      break;
     case SDLK_SPACE:
       code = SFEVENT_FIRE;
       break;
